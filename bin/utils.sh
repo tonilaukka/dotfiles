faee047d9ee2fcde1449ceed55f5afb930b903d9
Toni Laukka <toni@laukka.fi>
2016-03-12 20:58:28 +0200

    Print and execute funtions

---
+++ bin/utils.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+execute() {
+	eval "$1" &> /dev/null
+	print_result $? "${2:-$1}"
+}
+
+print_error() {
+    print_in_red "  [✖] $1 $2\n"
+}
+
+print_in_green() {
+    printf "\e[0;32m$1\e[0m"
+}
+
+print_in_purple() {
+    printf "\e[0;35m$1\e[0m"
+}
+
+print_in_red() {
+    printf "\e[0;31m$1\e[0m"
+}
+
+print_in_yellow() {
+    printf "\e[0;33m$1\e[0m"
+}
+
+print_info() {
+    print_in_purple "\n $1\n\n"
+}
+
+print_question() {
+    print_in_yellow "  [?] $1"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    return $1
+}
+
+print_success() {
+    print_in_green "  [✔] $1\n"
+}
