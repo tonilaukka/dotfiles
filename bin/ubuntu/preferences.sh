d9ad834cd25257f6720714fe3de487dbeaa8b974
Toni Laukka <toni@laukka.fi>
2016-03-13 07:37:10 +0200

    Refactored repeat-interval and delay to constants

--- bin/ubuntu/preferences.sh
+++ bin/ubuntu/preferences.sh
@@ -3,16 +3,19 @@
 cd "$(dirname "$BASH_SOURCE")" \
 	&& source '../utils.sh'
 
+declare -r REPEAT_INTERVAL='25'
+declare -r DELAY='400'
+
 set_preferences() {
-	execute 'gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25 && 
-		 gsettings set org.gnome.desktop.peripherals.keyboard delay 400' \
-		'Change keyboard repeat-interval and delay'	
+	execute "gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval $1 && 
+		 gsettings set org.gnome.desktop.peripherals.keyboard delay $2" \
+		"Changed keyboard repeat-interval to $REPEAT_INTERVAL ms and delay to $DELAY ms"	
 }
 
 main() {
 
 	print_in_purple '\n Ubuntu preferences\n'
-	set_preferences
+	set_preferences $REPEAT_INTERVAL $DELAY
 
 }
 
