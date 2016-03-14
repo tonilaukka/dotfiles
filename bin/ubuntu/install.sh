e0cacaf33006f35a3063ba08e7ff8e784ec66f10
Toni Laukka <toni@laukka.fi>
2016-03-14 12:48:02 +0200

    Added Caps Lock and Mouse Scrolling to TODO

--- bin/ubuntu/install.sh
+++ bin/ubuntu/install.sh
@@ -3,6 +3,13 @@
 cd "$(dirname "$BASH_SOURCE")" \
 	&& source '../utils.sh'
 
+# TODO
+# - xcape to caps lock to esc/ctrl
+# 	- http://www.economyofeffort.com/2014/08/11/beyond-ctrl-remap-make-that-caps-lock-key-useful/
+# - adjust mouse scrolling speed
+#	- https://github.com/pwr/Solaar/issues/158
+#	- xinput set-prop "Logitech Performance MX" "Evdev Scrolling Distance" 1 10 10
+
 main() {
 	print_info '\n Install Ubuntu packages'
 }
